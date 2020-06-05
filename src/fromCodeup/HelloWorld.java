9fcf41fad68f4279b73ffd72b0e715fe9387a5ed
SUNG IL LEE <sungil.lee90@gmail.com>
2020-06-05 15:25:05 -0500

    Completed

--- src/fromCodeup/HelloWorld.java
+++ src/fromCodeup/HelloWorld.java
@@ -1,7 +1,11 @@
 package fromCodeup;
 
 public class HelloWorld {
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.print("Hello,World! with print");
+        System.out.print("Hello,World! with print");
     }
+
 }
