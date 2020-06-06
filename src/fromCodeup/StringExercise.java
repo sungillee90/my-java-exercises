f2776ff3a62dcf27b680ef0585ee4e34e8323e92
SUNG IL LEE <sungil.lee90@gmail.com>
2020-06-06 10:16:18 -0500

    Complete part 1 of the exercise in Strings.

---
+++ src/fromCodeup/StringExercise.java
@@ -0,0 +1,15 @@
+package fromCodeup;
+
+public class StringExercise {
+    public static void main(String[] args) {
+        String printThis = "We don't need no education\n" +
+                            "We don't need no thought control";
+        System.out.println(printThis);
+
+        String checkItOut = "Check \"this\" out!. \"s inside of \"s!";
+        System.out.println(checkItOut);
+
+        String usingBackslash = "In windows, the main drive is usually C:\\";
+        System.out.println(usingBackslash);
+    }
+}
