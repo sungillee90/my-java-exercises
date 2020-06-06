a11a882b6c8bb74c1fe6809ca5d24e6d74528e18
SUNG IL LEE <sungil.lee90@gmail.com>
2020-06-06 10:25:35 -0500

    Complete Strings Exercise.

---
+++ src/Bob.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What will you say to Bob?");
+        String userInput = sc.nextLine();
+
+        if (userInput.endsWith("?")) {
+            System.out.println("Sure");
+        } else if (userInput.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        } else if (userInput.equals("")) {
+            System.out.println("Fine, be that way!");
+        } else {
+            System.out.println("Whatever!");
+        }
+    }
+}
