0eb13a821ae1eded385d9138c734e04a6c54f791
SUNG IL LEE <sungil.lee90@gmail.com>
2020-08-13 13:31:31 -0500

    finished BMI calculator exercise

---
+++ src/practice/BMICalc.java
@@ -0,0 +1,20 @@
+package practice;
+
+import java.util.Scanner;
+
+public class BMICalc {
+    public static void main(String[] args) {
+        float height;
+        int weight;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your height in meter: ");
+        height = Float.parseFloat(sc.nextLine());
+        System.out.println("Enter your weight in kg: ");
+        weight= Integer.parseInt(sc.nextLine());
+        float calcBMI = weight / (height * height);
+        float BMI = (float) (Math.round(calcBMI *100)/100.0);
+        System.out.println("Your BMI is = " + BMI);
+
+    }
+}
