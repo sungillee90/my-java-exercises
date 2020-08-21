d5288b238cad80939800d4001c3fdd672591ff63
SUNG IL LEE <sungil.lee90@gmail.com>
2020-08-21 15:56:01 -0500

    redid FizzBuzz

--- src/fromCodeup/ControlFlowExercises.java
+++ src/fromCodeup/ControlFlowExercises.java
@@ -1,5 +1,8 @@
 package fromCodeup;
 
+import org.w3c.dom.Node;
+
+import javax.swing.tree.TreeNode;
 import java.util.*;
 
 public class ControlFlowExercises {
@@ -230,5 +233,107 @@ public class ControlFlowExercises {
 //            System.out.print(alist.get(i) + " ");
 //        }
 //    }
+
+//    public Node balancedTree(ArrayList<Integer> inputList) {
+//        int arr[] = new int[] {1, 2, 3, 4, 5, 6, 7};
+//        int n = arr.length;
+//    }
+//    class Node {
+//
+//        int data;
+//        Node left, right;
+//
+//        Node(int d) {
+//            data = d;
+//            left = right = null;
+//        }
+//    }
+//
+//    class BinaryTree {
+//
+//        static Node root;
+//
+//        Node sortedArrayToBST(int arr[], int start, int end) {
+//
+//        /* Base Case */
+//        if (start > end) {
+//            return null;
+//        }
+//
+//        /* Get the middle element and make it root */
+//        int mid = (start + end) / 2;
+//        Node node = new Node(arr[mid]);
+//
+//        /* Recursively construct the left subtree and make it
+//         left child of root */
+//        node.left = sortedArrayToBST(arr, start, mid - 1);
+//
+//        /* Recursively construct the right subtree and make it
+//         right child of root */
+//        node.right = sortedArrayToBST(arr, mid + 1, end);
+//
+//        return node;
+//    }
+static class Node {
+
+    int data;
+    Node left, right;
+
+    Node(int d) {
+        data = d;
+        left = right = null;
+    }
+}
+
+    static class BinaryTree {
+
+        static Node root;
+
+        /* A function that constructs Balanced Binary Search Tree
+         from a sorted array */
+        Node sortedArrayToBST(int arr[], int start, int end) {
+
+            /* Base Case */
+            if (start > end) {
+                return null;
+            }
+
+            /* Get the middle element and make it root */
+            int mid = (start + end) / 2;
+            Node node = new Node(arr[mid]);
+
+        /* Recursively construct the left subtree and make it
+         left child of root */
+            node.left = sortedArrayToBST(arr, start, mid - 1);
+
+        /* Recursively construct the right subtree and make it
+         right child of root */
+            node.right = sortedArrayToBST(arr, mid + 1, end);
+
+            return node;
+        }
+
+        /* A utility function to print preorder traversal of BST */
+        void preOrder(Node node) {
+            if (node == null) {
+                return;
+            }
+            System.out.print(node.data + " ");
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+        public static void main(String[] args) {
+            BinaryTree tree = new BinaryTree();
+            int arr[] = new int[]{1, 2, 3, 4, 5, 6, 7};
+            int n = arr.length;
+            root = tree.sortedArrayToBST(arr, 0, n - 1);
+            System.out.println("Preorder traversal of constructed BST");
+            tree.preOrder(root);
+        }
+
+    }
+
+
+
 
 }
