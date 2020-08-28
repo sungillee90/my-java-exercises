63b2e2160c4c240c306281da67d7cae2c7c99883
SUNG IL LEE <sungil.lee90@gmail.com>
2020-08-28 11:11:51 -0500

    finished Binary conversion

---
+++ src/practice/BinaryDecimal.java
@@ -0,0 +1,15 @@
+package practice;
+
+import java.util.Scanner;
+
+public class BinaryDecimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a int number to convert to Binary");
+        int userInputInt = Integer.parseInt(sc.nextLine());
+
+        String s = Integer.toBinaryString(userInputInt);
+        System.out.println("To Binary = " + s);
+        
+    }
+}
