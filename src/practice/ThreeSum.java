c5798ad7949223112ba4fba56f39241ebbd54560
SUNG IL LEE <sungil.lee90@gmail.com>
2020-09-08 15:13:11 -0500

    init for 3sum

---
+++ src/practice/ThreeSum.java
@@ -0,0 +1,15 @@
+package practice;
+
+import java.util.List;
+
+public class ThreeSum {
+    // find all the set that sum of zero, UNIQ also
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+    }
+}
