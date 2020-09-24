ac9c381368be22cbf3443954c6846412acf4212a
SUNG IL LEE <sungil.lee90@gmail.com>
2020-09-24 13:30:58 -0500

    init setup for Shape

--- src/practice/PrototypePattern/Shape.java
+++ src/practice/PrototypePattern/Shape.java
@@ -2,8 +2,19 @@ package practice.PrototypePattern;
 
 public class Shape implements Cloneable{
 
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     @Override
     protected Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
+
 }
