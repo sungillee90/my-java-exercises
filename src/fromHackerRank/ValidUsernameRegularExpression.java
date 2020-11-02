ea22f0490b35dd42f59d1849d61fa4632373c818
SUNG IL LEE <sungil.lee90@gmail.com>
2020-11-02 14:55:43 -0600

    Completed Tag Content Extractor

--- src/fromHackerRank/ValidUsernameRegularExpression.java
+++ src/fromHackerRank/ValidUsernameRegularExpression.java
@@ -1,25 +1,25 @@
-package fromHackerRank;
-import java.util.Scanner;
-
-public class ValidUsernameRegularExpression {
-    class UsernameValidator {
-        public static final String regularExpression = "^[a-zA-Z]\\w{7,29}$";
-    }
-    public class Solution {
-        private static final Scanner scan = new Scanner(System.in);
-
-        public static void main(String[] args) {
-            int n = Integer.parseInt(scan.nextLine());
-            while (n-- != 0) {
-                String userName = scan.nextLine();
-
-                if (userName.matches(ValidUsernameRegularExpression.UsernameValidator.regularExpression)) {
-                    System.out.println("Valid");
-                } else {
-                    System.out.println("Invalid");
-                }
-            }
-        }
-    }
-}
-
+//package fromHackerRank;
+//import java.util.Scanner;
+//
+//public class ValidUsernameRegularExpression {
+//    class UsernameValidator {
+//        public static final String regularExpression = "^[a-zA-Z]\\w{7,29}$";
+//    }
+//    public class Solution {
+//        private static final Scanner scan = new Scanner(System.in);
+//
+//        public static void main(String[] args) {
+//            int n = Integer.parseInt(scan.nextLine());
+//            while (n-- != 0) {
+//                String userName = scan.nextLine();
+//
+//                if (userName.matches(ValidUsernameRegularExpression.UsernameValidator.regularExpression)) {
+//                    System.out.println("Valid");
+//                } else {
+//                    System.out.println("Invalid");
+//                }
+//            }
+//        }
+//    }
+//}
+//
