fc00defc6c0da2fb095f2526a667af3a73682127
SUNG IL LEE <sungil.lee90@gmail.com>
2020-11-17 10:28:21 -0600

    Completed String Token problem

--- src/fromHackerRank/JavaIfElsePrac.java
+++ src/fromHackerRank/JavaIfElsePrac.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class JavaIfElsePrac {
     public static void main(String[] args) {
-        Scanner sc = new Scanner();
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String ans = "";
         if (n % 2 == 1) {
