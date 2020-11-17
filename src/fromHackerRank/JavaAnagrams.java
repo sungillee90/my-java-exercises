662b44cb9c7245bd467eae3c590cbc0a1d624d0f
SUNG IL LEE <sungil.lee90@gmail.com>
2020-11-17 10:03:13 -0600

    Completed Anagrams problem

--- src/fromHackerRank/JavaAnagrams.java
+++ src/fromHackerRank/JavaAnagrams.java
@@ -1,11 +1,9 @@
 package fromHackerRank;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class JavaAnagrams {
     static boolean isAnagram(String a, String b) {
         // Complete the function
-
         int a_length = a.length();
         int b_length = b.length();
 
@@ -16,34 +14,41 @@ public class JavaAnagrams {
         a = a.toLowerCase();
         b = b.toLowerCase();
 
-//        char[] a_chars = a.toCharArray();
-//        char[] b_chars = b.toCharArray();
-//
-//        Arrays.sort(a_chars);
-//        Arrays.sort(b_chars);
-//
-//        if (a_chars.equals((b_chars)) return true;
-//        else return false;
+        int[] char_frequencies = new int[26];
 
-        int[] char_freq = new int[26];
-        for (int i=0; i<a_length; i++) {
+        for (int i = 0; i < a_length; i++) {
             char current_char = a.charAt(i);
             int index = current_char - 'a';
-            char_freq[index]++;
+            char_frequencies[index]++;
         }
 
-        for (int i=0; i<b_length; i++) {
-            char current_char = a.charAt(i);
+        for (int j = 0; j < b_length; j++) {
+            char current_char = b.charAt(j);
             int index = current_char - 'a';
-            char_freq[index]--;
+            char_frequencies[index]--;
         }
 
-        for (int i=0; i<26; i++) {
-            if (char_freq[i] != 0) {
+        for (int i = 0; i < 26; i++) {
+            if (char_frequencies[i] != 0) {
                 return false;
             }
         }
         return true;
+
+        // if (a_length != b_length) {
+        //     return false;
+        // }
+
+        // a = a.toLowerCase();
+        // b = b.toLowerCase();
+
+        // char[] a_chars = a.toCharArray();
+        // char[] b_chars = b.toCharArray();
+
+        // Arrays.sort(a_chars);
+        // Arrays.sort(b_chars);
+
+        // return Arrays.equals(a_chars, b_chars);
     }
 
     public static void main(String[] args) {
@@ -56,3 +61,4 @@ public class JavaAnagrams {
         System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
     }
 }
+
