075410b0bd62ab409e2f7716a0612960bf18ac56
SUNG IL LEE <sungil.lee90@gmail.com>
2020-11-24 19:55:06 -0600

    WIP solving Java SHA 256 problem

--- src/fromHackerRank/Solution.java
+++ src/fromHackerRank/Solution.java
@@ -1,14 +1,16 @@
 package fromHackerRank;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
-public class JavaSHA_256 {
+
+public class Solution {
+
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-
         System.out.println(getSHAHEX(s));
         sc.close();
     }
@@ -19,7 +21,7 @@ public class JavaSHA_256 {
             byte[] result = md.digest(s.getBytes());
             for (int i = 0; i < result.length; i++) {
                 String hex = Integer.toHexString(0xff & result[i]);
-                if (hex.length()==1) {
+                if (hex.length() == 1) {
                     sb.append('0');
                     sb.append(hex);
                 }
