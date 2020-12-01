7b06d589c8ff85ad748a418757dfa5ceceebfe3e
SUNG IL LEE <sungil.lee90@gmail.com>
2020-12-01 13:47:10 -0600

    Completed BigInteger

---
+++ src/fromHackerRank/JavaBigInteger.java
@@ -0,0 +1,16 @@
+package fromHackerRank;
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class JavaBigInteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger a = new BigInteger(sc.next());
+        BigInteger b = new BigInteger(sc.next());
+
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
