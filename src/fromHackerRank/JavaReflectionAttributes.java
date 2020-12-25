a783fe30a9655b80ed029373fa231ee6539cb120
SUNG IL LEE <sungil.lee90@gmail.com>
2020-12-25 10:20:22 -0600

    added boundary to the problem

--- src/fromHackerRank/JavaReflectionAttributes.java
+++ src/fromHackerRank/JavaReflectionAttributes.java
@@ -1,18 +1,18 @@
-package fromHackerRank;
-
-public class JavaReflectionAttributes {
-    public static void main(String[] args){
-        Class student = new Student().getClass();
-        Method[] methods = student.getDeclaredMethods();
-
-        ArrayList<String> methodList = new ArrayList<>();
-        for(Method m:methods){
-            methodList.add(m.getName());
-        }
-        Collections.sort(methodList);
-        for(String name: methodList){
-            System.out.println(name);
-        }
-    }
-
-}
+//package fromHackerRank;
+//
+//public class JavaReflectionAttributes {
+//    public static void main(String[] args){
+//        Class student = new Student().getClass();
+//        Method[] methods = student.getDeclaredMethods();
+//
+//        ArrayList<String> methodList = new ArrayList<>();
+//        for(Method m:methods){
+//            methodList.add(m.getName());
+//        }
+//        Collections.sort(methodList);
+//        for(String name: methodList){
+//            System.out.println(name);
+//        }
+//    }
+//
+//}
