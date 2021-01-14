a305918e584e7a2fc6b5db813f7673094665bec9
SUNG IL LEE <sungil.lee90@gmail.com>
2021-01-14 06:54:26 -0600

    added String Builder to the practice

--- src/practiceJava/StringBufferPrac.java
+++ src/practiceJava/StringBufferPrac.java
@@ -11,6 +11,13 @@ public class StringBufferPrac {
 
         buffer.append(" java, Welcome to learn new language " + userInput);
         System.out.println(buffer);
+
+        StringBuilder builder=new StringBuilder("hello");
+        builder.append("java");
+        System.out.println(builder);
+
+        // Found out StringBuilder took less time (Performance wise use StringBuilder,
+        // shorter time to loop through
     }
 }
 
