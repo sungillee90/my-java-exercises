a53ba6cee356f0d2381e7994aee00edc68fe88de
SUNG IL LEE <sungil.lee90@gmail.com>
2021-01-13 20:38:20 -0600

    practice the string builder

---
+++ src/practiceJava/StringBuilderPrac.java
@@ -0,0 +1,10 @@
+package practiceJava;
+
+public class StringBuilderPrac {
+    public static void main(String[] args) {
+        StringBuffer buffer=new StringBuffer("Hello");
+        buffer.append("java");
+        System.out.println(buffer);
+    }
+}
+
