43bd9977744b29d7f477e789bbfde631484a340c
SUNG IL LEE <sungil.lee90@gmail.com>
2021-02-11 16:26:03 -0600

    practice reverse integer

--- src/practiceJava/Solution.java
+++ src/practiceJava/Solution.java
@@ -1,34 +1,23 @@
 package practiceJava;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 
 public class Solution {
-
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        System.out.println(getSHAHEX(s));
-        sc.close();
-    }
-    public static String getSHAHEX(String s) {
-        StringBuffer sb = new StringBuffer();
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] result = md.digest(s.getBytes());
-            for (int i = 0; i < result.length; i++) {
-                String hex = Integer.toHexString(0xff & result[i]);
-                if (hex.length() == 1) {
-                    sb.append('0');
-                    sb.append(hex);
-                }
-            }
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+        System.out.println("Enter the integer to reverse");
+        int userInput = Integer.parseInt(sc.next());
+
+        int reversed = 0;
+        int pop;
+
+        while (userInput != 0) {
+            pop = userInput % 10;
+            userInput /= 10;
+
+            reversed = (reversed * 10) + pop;
         }
-        return sb.toString();
+
     }
 }
